f4eff3ff6a57a87c14409549c9fa1f725036f649
joaolucasvl <joaovitorlucasdev@gmail.com>
2025-11-20 20:53:54 -0300

    dev

--- Program.cs
+++ Program.cs
@@ -36,11 +36,10 @@ builder.Services.AddCors(options =>
 var app = builder.Build();
 
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+
+app.UseSwagger(); 
+app.UseSwaggerUI();
+
 
 app.UseHttpsRedirection();
 app.UseCors(MyAllowSpecificOrigins);
